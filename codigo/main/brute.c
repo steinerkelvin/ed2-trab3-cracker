c95a21ddbbbd4a99ef3ad13f5e1722a4656c7899
Athus <athuscavalini@gmail.com>
2018-11-14 23:39:55 -0200

    brute finished

--- codigo/main/brute.c
+++ codigo/main/brute.c
@@ -1,45 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <math.h>
+#include "key.h"
 
-typedef unsigned short int bit;
-
-#define C 5
+int compare_key(Key k1, Key k2) {
+	for (int i = 0; i < C; i++) {
+		if ( k1.digit[i] > k2.digit[i] ) return 1;
+		if ( k1.digit[i] < k2.digit[i] ) return -1;
+	}
+	return 0;
+}
 
 void dec_to_bin(int num, unsigned short int* bin, int size){
-   for (int aux = size-1; aux >= 0; aux--) {
-      if (num % 2 == 0) bin[aux] = 0;
-      else bin[aux] = 1;
-      num = num / 2;
-   }
+	for (int aux = size-1; aux >= 0; aux--) {
+		if (num % 2 == 0) bin[aux] = 0;
+		else bin[aux] = 1;
+		num = num / 2;
+	}
 }
 
-unsigned char assoc_int_char(int a) {
-	if (a < 27 && a > 0) {
-		a = a + 97;
-		return a;
-	}
-	if (a > 26 && a  < 32) {
-		a = a + 48;
-		return a;
-	}
-	return 0;
-}
-
-int assoc_char_int(unsigned char a) {
-	if (a < 123 && a > 96) {
-		a = a - 97;
-		return a;
-	}
-	if (a > 47 && a  < 54) {
-		a = a - 48;
-		return a+26;
-	}
-	return 0;
-}
-
-int bin_to_int(bit* bin, int size) {
+int bin_to_dec(unsigned short int* bin, int size) {
 	int s = size;
 	int dec = 0;
 	int i = 0;
@@ -47,67 +27,38 @@ int bin_to_int(bit* bin, int size) {
 	return dec;
 }
 
-void bin_to_char(bit* result, char* word, int size) {
-	int j = 0;
-	for (int i = 0; i < size*C; i = i+C){
-		bit characterb[5] = {result[i],result[i+1],result[i+2],result[i+3],result[i+4]}; //ao inves de fazer 5x, fazer size vezes...
-		word[j] = assoc_int_char(bin_to_int(characterb,size));
-		j++;
-	}
-}
-
-void soma(bit* index, bit* total, int size, char** table) {
-	bit temp[size*C];
-	bit piece[size];
-	dec_to_bin(0,total,size*C);
+void bin_to_string(unsigned short int* bin, char* word) {
 	int k = 0;
-	for (int i = 0; i < size*C; i++){
-		if (index[i] == 1) {
-			for (int j = 0; j < size; j++) {
-				dec_to_bin(assoc_char_int(table[i][j]),piece,size);
-				temp[k] = piece[0];
-				temp[k+1] = piece[1];
-				temp[k+2] = piece[2];
-				temp[k+3] = piece[3];
-				temp[k+4] = piece[4]; //ao inves de fazer 5x, deve-se fazer size vezes...
-				k = k+5;
-			}
-			dec_to_bin((bin_to_int(total,size*C) + bin_to_int(temp,size*C)),total,size*C);
-			k = 0;
-		}
+	unsigned short int characterb[B];
+	for (int i = 0; i < N; i = i+B){
+		int aux = i;
+		for (int j = 0; j < B; j++) characterb[j] = bin[aux++];
+		word[k] = ALPHABET[bin_to_dec(characterb,B)];
+		k++;
 	}
 }
 
-int main(int argc, char** argv){
+void main(int argc, char** argv) {
 
-	int size = strlen(argv[1]);
-
-	char** table = malloc(size*C*sizeof(char*));
-	for (int i = 0; i < size*C; i++) {
-		table[i] = malloc(size*sizeof(char));
-		scanf("%s",table[i]);
+	Key table[N];
+	for (int i = 0; i < N; i++) {
+		char aux[C];
+		scanf("%s",aux);
+		table[i] = init_key(aux);
 	}
 
-	bit bin[C*size];
-	bit result[C*size];
-	dec_to_bin(0,result,size*C);
-	char word[size];
+	Key in = init_key(argv[1]);
+	int size = pow(32,B);
 
-	long long int comb = pow(32,size);
+	unsigned short int comb[N];
+	char word[C];
 
-	for (int i = 0; i < comb; i++) {
-		dec_to_bin(i,bin,size*C);
-		soma(bin,result,size,table);
-		bin_to_char(result,word,size);
-		if (strcmp(word,argv[1])==0) {
-			printf("ACHOU\n");
-			char passwd[size];
-			bin_to_char(bin,passwd,size);
-			printf("%s\n",passwd);
-		}
+	for (int i = 0; i < size; i++) {
+		dec_to_bin(i,comb,N);
+		bin_to_string(comb,word);
+		Key kword = init_key(word);
+		Key sum = subset_sum(kword,table);
+		if (compare_key(sum,in) == 0) print_key_char(kword);
 	}
 
-	// dar free na table alocada dinamicamente;
-
-	return 0;
 }
