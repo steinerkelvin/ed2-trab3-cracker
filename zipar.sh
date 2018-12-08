f7a2fb2373fdeca665f7d37166e4dbc352d294c1
Kelvin Steiner Santos <kelvinsteinersantos@gmail.com>
2018-12-07 23:17:00 -0200

    movendo arquivos; zipador

---
+++ zipar.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+NAME="ed2-trab3-AthusCavalini-KelvinSantos"
+
+FILE="$NAME.zip"
+
+if [ -f "$FILE" ]; then
+    rm "$FILE"
+fi
+
+mkdir -p "$NAME/"
+
+ln -s -r -T "codigo/" "$NAME/"
+# ln -s -r -T "doc/" "$NAME/doc"
+
+zip "$FILE" -r  "$NAME/"
+
+rm -r "$NAME"
